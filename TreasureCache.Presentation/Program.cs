4f93937aff6aaf63d467875b46f7c36fcf615719
slik margor <slikmargor@gmail.com>
2023-10-29 12:39:12 +0100

    feat(rebase) : rebasing develop to mainstructure

--- TreasureCache.Presentation/Program.cs
+++ TreasureCache.Presentation/Program.cs
@@ -1,21 +1,11 @@
-using TreasureCache.Abstractions;
-using TreasureCache.Abstractions.Mediator.Extensions;
-using TreasureCache.Application;
-using TreasureCache.Core;
 using TreasureCache.Infrastructure;
-using TreasureCache.Presentation;
+using Tailwind;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.InstallInfrastructure(builder.Configuration);
-builder.Services.AddMediator(cfg => 
-    cfg.RegisterFromAssembliesContainingMarkers(new []
-    {
-        typeof(IInfrastructureMarker), typeof(IAbstractionMarker), typeof(ICoreMarker),
-        typeof(IPresentationMarker), typeof(IApplicationMarker)
-    }));
 
 var app = builder.Build();
 
